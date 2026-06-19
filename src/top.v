9dc9efaf183d8114de333ea2db4715444eb35108
Vipul Sharma <146561309+vipul3112@users.noreply.github.com>
2026-06-19 16:13:26 +0530

    Update top.v
    Top module

--- src/top.v
+++ src/top.v
@@ -1,1 +1,20 @@
+module AES(out,in,key,mode,Clk);
+input [127:0]in,key;
+input Clk,mode;
+output reg [127:0] out;
+wire [127:0]cipher,inv_cipher;
+
+Encryption e0(cipher,in,key,Clk);
+
+Decryption_new d0(inv_cipher,cipher,key,Clk);
+
+always @(*) begin
+if(mode==1'b0)
+     out<=cipher;
+else if(mode==1'b1)
+     out<= inv_cipher;
+ else
+  out<=0;
+end
+endmodule
 
